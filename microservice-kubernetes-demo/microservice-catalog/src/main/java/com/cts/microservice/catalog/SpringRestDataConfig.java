51a8c1b8a47e686a3e17948f4657a3eb96b578e2
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-21 06:09:35 -0400

    Update SpringRestDataConfig.java

--- microservice-kubernetes-demo/microservice-catalog/src/main/java/com/cts/microservice/catalog/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-catalog/src/main/java/com/cts/microservice/catalog/SpringRestDataConfig.java
@@ -1,4 +1,4 @@
-package com.microservice.catalog;
+package com.cts.microservice.catalog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
