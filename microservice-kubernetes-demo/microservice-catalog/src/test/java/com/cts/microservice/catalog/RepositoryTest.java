20b9829ce48cf3b52076f99b49cbde3d3d1d48b3
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-21 06:15:41 -0400

    Create RepositoryTest.java

---
+++ microservice-kubernetes-demo/microservice-catalog/src/test/java/com/cts/microservice/catalog/RepositoryTest.java
@@ -0,0 +1,29 @@
+package com.ewolff.microservice.catalog;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = CatalogApp.class, webEnvironment = WebEnvironment.NONE)
+@ActiveProfiles("test")
+public class RepositoryTest {
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@Test
+	public void AreAllIPodReturned() {
+
+		//assertThat(itemRepository.findByNameContaining("iPhone"), hasSize(3));
+		//assertTrue(itemRepository.findByNameContaining("iPhone").stream().anyMatch(s -> s.getName().equals("Samsung")));
+
+	}
+}
