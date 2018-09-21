c10fb2bd16dd59b075ed7a1e70c9476f76330483
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-21 06:08:18 -0400

    Add files via upload

---
+++ microservice-kubernetes-demo/microservice-catalog/src/main/java/com/cts/microservice/catalog/CatalogApp.java
@@ -0,0 +1,35 @@
+package com.microservice.catalog;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+@EnableAutoConfiguration
+@Component
+public class CatalogApp {
+
+	private final ItemRepository itemRepository;
+
+	@Autowired
+	public CatalogApp(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+
+	@PostConstruct
+	public void generateTestData() {
+		itemRepository.save(new Item("iPhone", 32670.0));
+		itemRepository.save(new Item("Samsung", 24280.0));
+		itemRepository.save(new Item("Asus", 15200.0));
+		itemRepository.save(new Item("Nokia", 18000.0));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApp.class, args);
+	}
+
+}
