1b057b88d27c9cd0ebbe758759ac748367aa4818
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-21 06:26:38 -0400

    Add files via upload

---
+++ microservice-kubernetes-demo/microservice-customer/src/main/java/com/cts/microservice/customer/CustomerApp.java
@@ -0,0 +1,34 @@
+package com.microservice.customer;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+@EnableAutoConfiguration
+@Component
+public class CustomerApp {
+
+	private final CustomerRepository customerRepository;
+
+	@Autowired
+	public CustomerApp(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	@PostConstruct
+	public void generateTestData() {
+                customerRepository.save(new Customer("Aditya", "kumar", "aditya05193@gmail.com", "Hinjewadi", "Pune"));
+		customerRepository.save(new Customer("Gaurav", "T.", "gaurav.t@gmail.com", "Wakad", "Pune"));
+                customerRepository.save(new Customer("Devendra", "brahmankar", "devendra.brahmankar@gmail.com", "Wakad", "Pune"));
+        }
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApp.class, args);
+	}
+
+}
