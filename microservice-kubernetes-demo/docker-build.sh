73a655c06437e12de209cdc6ed52a51392fc6e9c
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-27 04:26:27 -0400

    Update docker-build.sh

--- microservice-kubernetes-demo/docker-build.sh
+++ microservice-kubernetes-demo/docker-build.sh
@@ -2,23 +2,11 @@
 if [ -z "$DOCKER_ACCOUNT" ]; then
     DOCKER_ACCOUNT=aditya05193
 fi;
-docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
+docker build --tag=$DOCKER_ACCOUNT/microservice-catalog:v1 microservice-catalog
+docker push $DOCKER_ACCOUNT/microservice-catalog
 
-docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
+docker build --tag=$DOCKER_ACCOUNT/microservice-customer:v1 microservice-customer
+docker push $DOCKER_ACCOUNT/microservice-customer
 
-docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
-
-docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
-
-docker build --tag=microservice-kubernetes-demo-hystrix-dashboard microservice-kubernetes-demo-hystrix-dashboard
-docker tag microservice-kubernetes-demo-hystrix-dashboard $DOCKER_ACCOUNT/microservice-kubernetes-demo-hystrix-dashboard:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-hystrix-dashboard
-
+docker build --tag=$DOCKER_ACCOUNT/microservice-order:v1 microservice-order
+docker push $DOCKER_ACCOUNT/microservice-order
