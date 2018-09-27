72a9ba241ee3474e810042088090062803ab1986
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-27 03:44:05 -0400

    Update SpringRestDataConfig.java

--- microservice-kubernetes-demo/microservice-customer/src/main/java/com/cts/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-customer/src/main/java/com/cts/microservice/customer/SpringRestDataConfig.java
@@ -1,4 +1,4 @@
-package com.microservice.customer;
+package com.cts.microservice.customer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
